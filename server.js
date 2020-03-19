5609dddfa1ae8d2778a5b2cef3bce07a7e214543
Gabriel Ruschel <grc15@inf.ufpr.br>
2020-03-19 15:15:45 -0300

    Initial files, intial server file and database config

---
+++ server.js
@@ -0,0 +1,46 @@
+const hostname = '127.0.0.1';
+const port = 8000;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const databaseConfig = require('./config/database.js');
+const mongoose = require('mongoose');
+
+// Carrega o modulo HTTP do Node
+var http = require("http");
+
+const app = express();
+
+app.use(bodyParser.json())
+
+require('./app/routes/poll.routes.js')(app);
+
+// listen for requests
+app.listen(port,hostname, () => {
+    console.log("Server is listening on port 8000");
+});
+
+mongoose.Promise = global.Promise;
+
+//Connecting to the database
+mongoose.connect(databaseConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log("Could not connect to the database. Err ... ",err);
+    process.exit();
+});
+
+// http.createServer((req,res) => {
+//     if (req.url == '/' && req.method == 'GET') {
+//         res.writeHead(200, {'Content-Type': 'application/json'});
+//         var json = {"message": "Olá mundo!"}
+//         res.write(JSON.stringify(json));
+//         res.end();
+//     }
+// })
+// .listen(port,hostname, () => {
+//     console.log("Servidor executando em http://127.0.0.1:8000/");
+// })
